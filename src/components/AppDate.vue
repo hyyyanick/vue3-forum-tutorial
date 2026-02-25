c7b3d673115a017dfc9df39a0cf8cc50ce6c6e49
Yannick <huyiyan1986@gmail.com>
2026-02-25 22:46:00 +0100

    added app date component as base

---
+++ src/components/AppDate.vue
@@ -0,0 +1,33 @@
+<script setup>
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+dayjs.extend(relativeTime)
+dayjs.extend(localizedFormat)
+
+defineProps({
+  timestamp: {
+    type: Number,
+    required: true
+  }
+})
+
+function diffFromNow (date) {
+  return dayjs.unix(date).fromNow()
+}
+
+function humanFriendlyDate (date) {
+  return dayjs.unix(date).format('llll')
+}
+
+</script>
+
+<template>
+  <span :title="humanFriendlyDate(timestamp)">
+    {{ diffFromNow(timestamp) }}
+  </span>
+</template>
+
+<style scoped>
+
+</style>
