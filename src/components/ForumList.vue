e0081d4b3a054596d86d58922f2b1be69e961ca9
Yannick <huyiyan1986@gmail.com>
2026-02-25 23:02:16 +0100

    added forum list to home page

---
+++ src/components/ForumList.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({
+  forums: {
+    type: Array,
+    required: true
+  }
+})
+
+function forumThreadsWord (forum) {
+  if (forum.threads?.length) {
+    return forum.threads?.length === 1 ? 'thread' : 'threads'
+  } else {
+    return 'no threads'
+  }
+}
+
+</script>
+
+<template>
+  <div class="col-full">
+    <div class="forum-list">
+      <h2 class="list-title">
+        Forums
+      </h2>
+
+      <div class="forum-listing" v-for="forum in forums" :key="forum.id">
+        <div class="forum-details">
+          <router-link
+            :to="{ name: 'Forum', params: { forumId: forum.id }}"
+            class="text-xlarge"
+          >
+            {{ forum.name }}
+          </router-link>
+          <p>{{ forum.description }}</p>
+        </div>
+
+        <div class="threads-count">
+          <p>
+            <span class="count"> {{ forum.threads?.length }}</span>
+            {{ forumThreadsWord(forum) }}
+          </p>
+        </div>
+
+        <div class="last-thread">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
